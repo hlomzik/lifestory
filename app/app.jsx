e22178378f168d1a18a4e9dce6a97dc75ec1c2ae
hlomzik <hlomzik@gmail.com>
2017-02-19 15:48:40 +0300

    Use one list for all events
    But we still use stub events.

--- app/app.jsx
+++ app/app.jsx
@@ -3,20 +3,21 @@ import React from 'react'
 
 import Events from './Events'
 
-const event = {
-  type: 'single',
-  date: '2017-02-09T23:00:00Z',
-  title: 'Start the Story of My Life'
-}
-
-const cont = {
-  type: 'continuous',
-  from: '2017-02-06T00:00:00Z',
-  to: '2017-02-11T00:00:00Z',
-  title: 'Just a work week in February'
-}
+const events = [
+  {
+    type: 'single',
+    date: '2017-02-09T23:00:00Z',
+    title: 'Start the Story of My Life'
+  },
+  {
+    type: 'continuous',
+    from: '2017-02-06T00:00:00Z',
+    to: '2017-02-11T00:00:00Z',
+    title: 'Just a work week in February'
+  }
+]
 
 ReactDOM.render(
-  <Events from="2017-02-01" to="2017-02-11" length={300} events={[ event, cont ]} />,
+  <Events from="2017-02-01" to="2017-02-11" length={300} events={events} />,
   document.querySelector('.events')
 )
